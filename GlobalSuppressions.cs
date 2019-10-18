46d0c5c62c8f07ccee957931ad6f91da671d3f2d
Andrey Gruber <andrey.gruber@gmail.com>
2019-10-18 13:36:39 +0300

    Added GlobalSuppressions file

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "CA2100:Review SQL queries for security vulnerabilities", Justification = "<Pending>", Scope = "member", Target = "~M:ag.DbData.Abstraction.DbDataObject.innerExecute(System.String,System.Int32,System.Boolean)~System.Int32")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "CA2100:Review SQL queries for security vulnerabilities", Justification = "<Pending>", Scope = "member", Target = "~M:ag.DbData.Abstraction.DbDataObject.innerGetDataReader(System.String,System.Int32)~System.Data.Common.DbDataReader")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "CA2100:Review SQL queries for security vulnerabilities", Justification = "<Pending>", Scope = "member", Target = "~M:ag.DbData.Abstraction.DbDataObject.innerGetScalar(System.String,System.Int32,System.Boolean)~System.Object")]
